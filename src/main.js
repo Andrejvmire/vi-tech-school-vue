adaee9d7996113d9b8f46349f5442943a7f55a53
Pavel Galkin <pavel.galkin@vseinstrumenti.ru>
2021-11-26 09:11:06 +0300

    login logic

--- src/main.js
+++ src/main.js
@@ -3,11 +3,35 @@ import App from './App.vue';
 import router from './router';
 import VueMaterial from 'vue-material';
 import './assets/scss/main.scss';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    token: '',
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+  },
+  actions: {
+    async getToken(context, payload) {
+      const response = await axios.post('http://localhost:1780/api/login', {
+        email: payload.email,
+        password: payload.password,
+      });
+      context.commit('setToken', response.data.data);
+    },
+  },
+});
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,99 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <md-card class="card" v-if="isSignIn">
+      <md-card-content>
+        <md-field>
+          <label>Email</label>
+          <md-input v-model="signIn.email"></md-input>
+        </md-field>
+        <md-field>
+          <label>Пароль</label>
+          <md-input v-model="signIn.plainPassword"></md-input>
+        </md-field>
+        <md-field>
+          <label>Пароль еще раз</label>
+          <md-input v-model="signIn.repeatedPlainPassword"></md-input>
+        </md-field>
+        <md-field>
+          <label>Как вас зовут</label>
+          <md-input v-model="signIn.fullName"></md-input>
+        </md-field>
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button @click="isSignIn = false">Войти</md-button>
+        <md-button @click="signInMethod">Отправить</md-button>
+      </md-card-actions>
+    </md-card>
+
+    <md-card class="card" v-else>
+      <md-card-content>
+        <md-field>
+          <label>Email</label>
+          <md-input v-model="email"></md-input>
+        </md-field>
+        <md-field>
+          <label>Пароль</label>
+          <md-input v-model="password"></md-input>
+        </md-field>
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button @click="loginMethod">Войти</md-button>
+        <md-button @click="isSignIn = true">Зарегистрироваться</md-button>
+      </md-card-actions>
+    </md-card>
+
+    <md-card class="card" v-if="error">
+      <md-card-content>
+        {{ error }}
+      </md-card-content>
+    </md-card>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      email: '',
+      password: '',
+      isSignIn: false,
+      signIn: {
+        email: '',
+        plainPassword: '',
+        repeatedPlainPassword: '',
+        fullName: '',
+      },
+      error: '',
+    };
+  },
+  methods: {
+    async signInMethod() {
+      try {
+        const response = await axios.post('http://localhost:1780/api/registration', this.signIn);
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async loginMethod() {
+      try {
+        this.$store.dispatch('getToken', { email: this.email, password: this.password });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.card {
+  max-width: 400px;
+  margin: 100px auto;
+}
+</style>
