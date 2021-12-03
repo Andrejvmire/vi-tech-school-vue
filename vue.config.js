04caa50f4b8dd31f41c0dea618683ff3f194e6ca
andrey <andrej_vmire@mail.ru>
2021-12-03 12:58:15 +0300

    добавлен sourceMap для css

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  configureWebpack: {
+    devtool: process.env.NODE_ENV === 'development'
+      ? 'inline-source-map'
+      : false,
+  },
+  css: {
+    sourceMap: process.env.NODE_ENV === 'development'
+  }
+}
